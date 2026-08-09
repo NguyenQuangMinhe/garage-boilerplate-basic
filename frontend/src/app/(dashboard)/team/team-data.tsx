25dcb29af275ecc86566ba83846d57341efc7daa
Perry <s4146473@student.rmit.edu.au>
2026-08-09 21:52:43 +1000

    feat(team-page): add placeholder image for missing team member photos. rename 'public/team-jpg' folder to 'public/team'

--- frontend/src/app/(dashboard)/team/team-data.tsx
+++ frontend/src/app/(dashboard)/team/team-data.tsx
@@ -10,31 +10,31 @@ export const teamMembers: [TeamMember, TeamMember, TeamMember, TeamMember, TeamM
     name: 'Reese Lucal Paul',
     role: 'Project Manager',
     blurb: 'Striving to help both my team and our clients get what they need.',
-    image: '/placeholder.png',
+    image: '/team/placeholder.svg',
   },
   {
     name: 'Perry Tsironis',
     role: 'Developer',
     blurb: 'Information Technology student with a passion for Data Science and problem solving in logical ways.',
-    image: '/team-jpg/perry.jpg',
+    image: '/team/perry.jpg',
   },
   {
     name: 'Nguyen Quang Minh',
     role: 'Developer',
     blurb: 'Always looking forward to learning new things and applying them.',
-    image: '/team-jpg/quang.png',
+    image: '/team/quang.png',
   },
   {
     name: 'Ming Yan Tan',
     role: 'Business Analyst',
     blurb: 'Turning business bottlenecks into clean requirements, fulfilling client requirements.',
-    image: '/placeholder.png',
+    image: '/team/placeholder.svg',
   },
   {
     name: 'Jamie Rottura',
     role: 'UX Designer',
     blurb: 'Always willing to help others and learn new skills.',
-    image: '/team-jpg/jamie.png',
+    image: '/team/jamie.png',
   }
 
 ];
